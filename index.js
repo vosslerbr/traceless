cfb1f4d6e0512c7ba753f64f37cedcd3de55c0f6
Brady Vossler <bradyvossler@gmail.com>
2020-01-06 14:27:46 -0600

    Fixed nav bug on mobile
    Slide-in nav would cover the hamburger button. Fixed so the button and logo move over with nav.

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ hamburgerMenu.addEventListener('click', function() {
     const logoHamburger = document.getElementById('logo-hamburger')
     if (nav.style.width === '220px') {
         nav.style.width = 0;
-        logoHamburger.style.marginRight = 0;
+        logoHamburger.style.left = 0;
     } else {
         nav.style.width = '220px';
-        logoHamburger.style.marginRight = '220px';
+        logoHamburger.style.left = '-220px';
     }
 });
